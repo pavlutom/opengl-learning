22ee405442322c0cc3ecb79afbeabe567386c6ee
Tomáš Pavlůsek <tomas.pavlusek@uptim.ai>
2026-03-29 14:30:25 +0200

    shaders

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,9 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 #include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
 
 
 #define WINDOW_WIDTH 800
@@ -14,18 +17,117 @@ void framebuffer_size_callback(GLFWwindow* window, int width, int height)
 	glViewport(0, 0, width, height);
 }
 
+
 // Input processing
-void processInput(GLFWwindow* window) {
+void processInput(GLFWwindow* window)
+{
 	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
 		glfwSetWindowShouldClose(window, true);
 	}
 }
 
+
+std::string readFileContents(const std::string& filePath)
+{
+	std::ifstream fileStream(filePath);
+
+	if (!fileStream.is_open())
+	{
+		std::cerr << "Could not read file " << filePath << ". File does not exist." << std::endl;
+		return "";
+	}
+
+	std::stringstream buffer;
+	buffer << fileStream.rdbuf();
+	return buffer.str();
+}
+
+
+// Compile shaders
+unsigned int compileShaders(std::string path)
+{
+	std::string vertSource = readFileContents(path + "/shader.vert");
+	std::string fragSource = readFileContents(path + "/shader.frag");
+
+	const char* vertSourceC = vertSource.c_str();
+	const char* fragSourceC = fragSource.c_str();
+
+	unsigned int vertShader, fragShader;
+	vertShader = glCreateShader(GL_VERTEX_SHADER);
+	fragShader = glCreateShader(GL_FRAGMENT_SHADER);
+
+	int successVert, successFrag;
+	char infoLog[512];
+
+	glShaderSource(vertShader, 1, &vertSourceC, NULL);
+	glCompileShader(vertShader);
+	glGetShaderiv(vertShader, GL_COMPILE_STATUS, &successVert);
+	if (!successVert)
+	{
+		glGetShaderInfoLog(vertShader, 512, NULL, infoLog);
+		std::cerr << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
+	}
+
+	glShaderSource(fragShader, 1, &fragSourceC, NULL);
+	glCompileShader(fragShader);
+	glGetShaderiv(fragShader, GL_COMPILE_STATUS, &successFrag);
+	if (!successFrag)
+	{
+		glGetShaderInfoLog(fragShader, 512, NULL, infoLog);
+		std::cerr << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
+	}
+
+	if (!successVert || !successFrag)
+	{
+		return NULL;
+	}
+
+	unsigned int shaderProgram;
+	shaderProgram = glCreateProgram();
+
+	glAttachShader(shaderProgram, vertShader);
+	glAttachShader(shaderProgram, fragShader);
+	glLinkProgram(shaderProgram);
+
+	glDeleteShader(vertShader);
+	glDeleteShader(fragShader);
+
+	return shaderProgram;
+}
+
+
 // Rendering
-void render() {
+void render(unsigned int VAO, unsigned int shaderProgram)
+{
+	// clear
 	glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
 	glClear(GL_COLOR_BUFFER_BIT);
+
+	// shaders
+	glUseProgram(shaderProgram);
+	
+	// bind VAO
+	glBindVertexArray(VAO);
+
+	// draw
+	glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+
+	// unbind VAO
+	glBindVertexArray(0);
 }
+
+
+float vertices[] = {
+	 0.5f,  0.5f, 0.0f,  // top right
+	 0.5f, -0.5f, 0.0f,  // bottom right
+	-0.5f, -0.5f, 0.0f,  // bottom left
+	-0.5f,  0.5f, 0.0f   // top left 
+};
+unsigned int indices[] = {  // note that we start from 0!
+	0, 1, 3,   // first triangle
+	1, 2, 3    // second triangle
+};
+
 
 int main()
 {
@@ -54,6 +156,38 @@ int main()
 	glViewport(0, 0, WINDOW_WIDTH, WINDOW_HEIGHT);
 	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
+	// Buffers
+	unsigned int VBO, VAO, EBO;
+	glGenBuffers(1, &VBO);
+	glGenVertexArrays(1, &VAO);
+	glGenBuffers(1, &EBO);
+
+	// bind VAO
+	glBindVertexArray(VAO);
+
+	// copy vertices array into vertex buffer
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	// copy index array into element buffer
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+	
+	// set the vertex attributes pointers
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
+
+	// unbind VAO
+	glBindBuffer(GL_ARRAY_BUFFER, NULL);
+
+	// Shaders
+	unsigned int defaultShaders = compileShaders("resources/shaders/default");
+	if (!defaultShaders)
+	{
+		std::cout << "Failed to compile shaders." << std::endl;
+		return -1;
+	}
+
 	// Main loop
 	while (!glfwWindowShouldClose(window))
 	{
@@ -61,7 +195,7 @@ int main()
 		processInput(window);
 
 		// Rendering
-		render();
+		render(VAO, defaultShaders);
 
 		// check and call events and swap the buffers
 		glfwSwapBuffers(window);
